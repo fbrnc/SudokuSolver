57572d962da13b2385ea725de4ad8cc84c527d3f
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2012-05-07 01:11:39 +0200

    Refactoring and added comments

--- Board.php
+++ Board.php
@@ -1,32 +1,28 @@
 <?php
 
+/**
+ * Sudoku board
+ *
+ * @author Fabrizio Branca
+ * @since 2012-05-06
+ */
 class Board {
 
+	/**
+	 * @var array
+	 */
 	protected $values = array();
 
+	/**
+	 * @var array
+	 */
 	protected $candidates = array();
 
-	public function getFirstCandidate() {
-		foreach (range(1, 9) as $y) {
-			foreach (range(1, 9) as $x) {
-				$candidates = $this->getCandidates($x, $y);
-				if (is_array($candidates)) {
-					return array(
-						'x' => $x,
-						'y' => $y,
-						'value' => current($candidates),
-					);
-				}
-			}
-		}
-		throw new Exception('No candidate found');
-	}
-
-	public function setValueFromFirstCandidate() {
-		$guess = $this->getFirstCandidate();
-		$this->setValue($guess['x'], $guess['y'], $guess['value']);
-	}
-
+	/**
+	 * Convenience method to initialize the board with a single string
+	 *
+	 * @param $string
+	 */
 	public function setValuesAsString($string) {
 		$string = preg_replace('/[^\.1-9]/', '', $string);
 		$counter = 0;
@@ -41,191 +37,119 @@ class Board {
 		}
 	}
 
+	/**
+	 * Set value for a given cell
+	 * and remove corresponding candidates from cells in same row, column and section.
+	 *
+	 * Before setting the value checks will be done if this is allowed
+	 *
+	 * @param $x
+	 * @param $y
+	 * @param $value
+	 * @throws Exception
+	 */
 	public function setValue($x, $y, $value) {
-		// echo "<b>Setting value $value to cell ($x, $y)</b><br />";
 		if (isset($this->values[$x][$y])) {
 			throw new Exception("Value for position ($x, $y) was set before.");
 		}
 
-		foreach ($this->getAllOtherCellsInColumn($x, $y) as $coordinate) {
+		$relatedCells = $this->getAllRelatedCells($x, $y);
+
+		// do checks if the current value already exists
+		foreach ($relatedCells as $coordinate) {
 			if ($this->getValue($coordinate['x'], $coordinate['y']) === $value) {
-				throw new Exception("Column check: Value $value is already present in cell ({$coordinate['x']}, {$coordinate['y']})");
-			}
-		}
-		foreach ($this->getAllOtherCellsInRow($x, $y) as $coordinate) {
-			if ($this->getValue($coordinate['x'], $coordinate['y']) === $value) {
-				throw new Exception("Row check: Value $value is already present in cell ({$coordinate['x']}, {$coordinate['y']})");
-			}
-		}
-		foreach ($this->getAllOtherCellsInSection($x, $y) as $coordinate) {
-			if ($this->getValue($coordinate['x'], $coordinate['y']) === $value) {
-				throw new Exception("Section check: Value $value is already present in cell ({$coordinate['x']}, {$coordinate['y']})");
+				throw new Exception("Value $value is already present in cell ({$coordinate['x']}, {$coordinate['y']})");
 			}
 		}
 
+		// setting the value
 		$this->values[$x][$y] = $value;
-		$this->candidates[$x][$y] = true;
 
-		$this->resolveCell($x, $y);
-	}
+		// resetting the candidates
+		$this->candidates[$x][$y] = false;
 
-	public function resolveCell($x, $y) {
-		$value = $this->getValue($x, $y);
-		if (!$value) {
-			throw new Exception("No value found for cell ($x, $y)");
-		}
-
-		// echo "Removing value $value from all cells of column $x<br />";
-		foreach ($this->getAllOtherCellsInColumn($x, $y) as $coordinate) {
-			$this->removeCandidate($coordinate['x'], $coordinate['y'], $value);
-		}
-		// echo "Removing value $value from all cells of row $y<br />";
-		foreach ($this->getAllOtherCellsInRow($x, $y) as $coordinate) {
-			$this->removeCandidate($coordinate['x'], $coordinate['y'], $value);
-		}
-		// echo "Removing value $value from all cells the section<br />";
-		foreach ($this->getAllOtherCellsInSection($x, $y) as $coordinate) {
+		foreach ($relatedCells as $coordinate) {
 			$this->removeCandidate($coordinate['x'], $coordinate['y'], $value);
 		}
 	}
 
-	public function getValue($x, $y) {
-		if (!isset($this->values[$x])) {
-			$this->values[$x] = array();
-		}
-		if (!isset($this->values[$x][$y])) {
-			$this->values[$x][$y] = null;
-		}
-		return $this->values[$x][$y];
-	}
-
-	public function getCandidates($x, $y) {
-		if (!isset($this->candidates[$x])) {
-			$this->candidates[$x] = array();
-		}
-		if (!isset($this->candidates[$x][$y])) {
-			$this->candidates[$x][$y] = range(1, 9);
-		}
-		return $this->candidates[$x][$y];
-	}
-
-	public function setCandidates($x, $y, array $candidates) {
-		if ($this->getCandidates($x, $y) === true) {
-			throw new Exception('Setting candidates for a cell that has been already solved is not allowed');
-		}
-		$this->candidates[$x][$y] = $candidates;
-	}
-
-	public function removeCandidate($x, $y, $candidate) {
-		$candidates = $this->getCandidates($x, $y);
-		if (is_array($candidates)) {
-			$key = array_search($candidate, $candidates);
-			if ($key !== false) {
-				$candidatesBefore = '{' . implode(', ', $candidates) . '}';
-				unset($candidates[$key]);
-				$candidatesAfter = '{' . implode(', ', $candidates) . '}';
-				// echo "Removing candidate $candidate from cell ($x, $y) $candidatesBefore -> $candidatesAfter<br />";
-
-				if (count($candidates) == 1) {
-					$this->setValue($x, $y, end($candidates));
-					// echo $this->printBoard();
-				} else {
-					$this->setCandidates($x, $y, $candidates);
+	/**
+	 * Resolve all cells as far as possible without guessing
+	 *
+	 * @return int candidate count
+	 * @throws Exception
+	 */
+	public function logicResolveAsFarAsPossible() {
+		$candidateCount = null;
+		while (!$this->isSolved()) {
+			foreach (range(1, 9) as $y) {
+				foreach (range(1, 9) as $x) {
+					$candidates = $this->getCandidates($x, $y);
+					if (is_array($candidates)) {
+						foreach ($candidates as $candidate) {
+							if ($this->isUniqueCandidate($x, $y, $candidate)) {
+								$this->setValue($x, $y, $candidate);
+							}
+						}
+					}
 				}
 			}
-		}
-	}
 
-	public function getAllOtherCellsInColumn($x, $y) {
-		$coordinates = array();
-		foreach (range(1, 9) as $key) {
-			if ($key != $y) {
-				$coordinates[] = array('x' => $x, 'y' => $key);
-			}
-		}
-		return $coordinates;
-	}
+			$oldCandidateCount = $candidateCount;
+			$candidateCount = $this->getTotalCandidateCount();
 
-	public function getAllOtherCellsInRow($x, $y) {
-		$coordinates = array();
-		foreach (range(1, 9) as $key) {
-			if ($key != $x) {
-				$coordinates[] = array('x' => $key, 'y' => $y);
-			}
-		}
-		return $coordinates;
-	}
-
-	public function getAllOtherCellsInSection($x, $y) {
-		$coordinates = array();
-		$xRange = array(
-			floor(($x-1)/3)*3+1,
-			floor(($x-1)/3)*3+3,
-		);
-		$yRange = array(
-			floor(($y-1)/3)*3+1,
-			floor(($y-1)/3)*3+3,
-		);
-		foreach (range($yRange[0], $yRange[1]) as $keyY) {
-			foreach (range($xRange[0], $xRange[1]) as $keyX) {
-				if ($keyX != $x || $keyY != $y) {
-					$coordinates[] = array('x' => $keyX, 'y' => $keyY);
-				}
-			}
-		}
-		return $coordinates;
-	}
-
-	public function isCandidateInCell($x, $y, $candidate) {
-		$candidates = $this->getCandidates($x, $y);
-		if (is_array($candidates)) {
-			// echo "Checking if $candidate is a candiate in ($x, $y)<br />";
-			return in_array($candidate, $candidates);
-		} else {
-			if ($this->getValue($x, $y) === $candidate) {
-				return true;
-				// throw new Exception("Trying a candidate that was already set before");
-			}
-			// echo "Checking if $candidate is a candiate in ($x, $y): No array<br />";
-		}
-		return false;
-	}
-
-	public function isUniqueCandidate($x, $y, $candidate) {
-		$isUnique = 'column';
-		// echo "Start checking if $candidate is candiate in column $x<br />";
-		foreach ($this->getAllOtherCellsInColumn($x, $y) as $coordinate) {
-			if ($this->isCandidateInCell($coordinate['x'], $coordinate['y'], $candidate)) {
-				// echo "Value $candidate from ($x, $y) is also candidate in ({$coordinate['x']}, {$coordinate['y']})<br />";
-				$isUnique = false;
+			if ($oldCandidateCount === $candidateCount) {
+				// echo 'Candidate count did not change. Aborting.<br />';
 				break;
 			}
 		}
-		if (!$isUnique) {
-			$isUnique = 'row';
-			// echo "Start checking if $candidate is candiate in row $y<br />";
-			foreach ($this->getAllOtherCellsInRow($x, $y) as $coordinate) {
-				if ($this->isCandidateInCell($coordinate['x'], $coordinate['y'], $candidate)) {
-					// echo "Value $candidate from ($x, $y) is also candidate in ({$coordinate['x']}, {$coordinate['y']})<br />";
-					$isUnique = false;
-					break;
-				}
-			}
-		}
-		if (!$isUnique) {
-			$isUnique = 'section';
-			// echo "Start checking if $candidate is candiate in section<br />";
-			foreach ($this->getAllOtherCellsInSection($x, $y) as $coordinate) {
-				if ($this->isCandidateInCell($coordinate['x'], $coordinate['y'], $candidate)) {
-					// echo "Value $candidate from ($x, $y) is also candidate in ({$coordinate['x']}, {$coordinate['y']})<br />";
-					$isUnique = false;
-					break;
-				}
-			}
-		}
-		return $isUnique;
+		return $candidateCount;
 	}
 
+	/**
+	 * Try guessing until along one candidate path
+	 * - until board is solved
+	 * - until error (Then remove guess from original candidates. Then try again...)
+	 *
+	 * @return void
+	 */
+	public function guess() {
+		$tryBoard = clone $this; /* @var $tryBoard Board */
+		while (!$tryBoard->isSolved()) {
+			$guess = $tryBoard->getFirstCandidate();
+			echo "Trying {$guess['value']} for ({$guess['x']}, {$guess['y']})<br />";
+			try {
+				$tryBoard->setValue($guess['x'], $guess['y'], $guess['value']);
+				$tryBoard->logicResolveAsFarAsPossible();
+				if ($tryBoard->isSolved()) {
+					// Case 1) Solved: OK, finished!
+					$this->setValue($guess['x'], $guess['y'], $guess['value']);$board = $tryBoard;
+					return;
+				}
+			} catch (Exception $e) {
+				// Case 2) Error: Remove current candidate from original board and try another one
+				echo "Did not work.<br />";
+				$this->removeCandidate($guess['x'], $guess['y'], $guess['value']);
+				return;
+			}
+			// Case 3) Not solved yet: Try additional guess ...
+		}
+	}
+
+	/**
+	 * Check if board is solved
+	 *
+	 * @return bool
+	 */
+	public function isSolved() {
+		return ($this->getTotalCandidateCount() == 0);
+	}
+
+	/**
+	 * Get html representation of current status
+	 *
+	 * @return string
+	 */
 	public function printBoard() {
 		$html = '<hr /><table>';
 		foreach (range(1, 9) as $y) {
@@ -247,19 +171,213 @@ class Board {
 		return $html;
 	}
 
-	public function getUnsolvedCellCount() {
-		$unsolvedCells = 0;
-		foreach (range(1, 9) as $x) {
-			foreach (range(1, 9) as $y) {
-				if (!$this->getValue($x, $y)) {
-					$unsolvedCells++;
+	/**
+	 * Get value for a given cell.
+	 * If the cell is not resolved yet null will be returned
+	 *
+	 * @param $x
+	 * @param $y
+	 * @return int|null
+	 */
+	protected function getValue($x, $y) {
+		if (!isset($this->values[$x])) {
+			$this->values[$x] = array();
+		}
+		if (!isset($this->values[$x][$y])) {
+			$this->values[$x][$y] = null;
+		}
+		return $this->values[$x][$y];
+	}
+
+	/**
+	 * Get candidates for a cell.
+	 * (And initialize candidates on first request)
+	 *
+	 * @param $x
+	 * @param $y
+	 * @return mixed
+	 */
+	protected function getCandidates($x, $y) {
+		if (!isset($this->candidates[$x])) {
+			$this->candidates[$x] = array();
+		}
+		if (!isset($this->candidates[$x][$y])) {
+			$this->candidates[$x][$y] = range(1, 9);
+		}
+		return $this->candidates[$x][$y];
+	}
+
+	/**
+	 * Remove candidate from a given cell.
+	 * If this results in a single candidate, the value will be set.
+	 *
+	 * @param $x
+	 * @param $y
+	 * @param $candidate
+	 */
+	protected function removeCandidate($x, $y, $candidate) {
+		$candidates = $this->getCandidates($x, $y);
+		if (is_array($candidates)) {
+			$key = array_search($candidate, $candidates);
+			if ($key !== false) {
+				unset($candidates[$key]);
+				if (count($candidates) == 1) {
+					$this->setValue($x, $y, end($candidates));
+				} else {
+					$this->candidates[$x][$y] = $candidates;
 				}
 			}
 		}
-		return $unsolvedCells;
 	}
 
-	public function getTotalCandidateCount() {
+	/**
+	 * Get all related cells
+	 *
+	 * @param $x
+	 * @param $y
+	 * @return array
+	 */
+	protected function getAllRelatedCells($x, $y) {
+		return array_merge(
+			$this->getAllOtherCellsInColumn($x, $y),
+			$this->getAllOtherCellsInRow($x, $y),
+			$this->getAllOtherCellsInSection($x, $y)
+		);
+	}
+
+	/**
+	 * Get all cells of the current column (given cell excluded)
+	 *
+	 * @param $x
+	 * @param $y
+	 * @return array
+	 */
+	protected function getAllOtherCellsInColumn($x, $y) {
+		$coordinates = array();
+		foreach (range(1, 9) as $key) {
+			if ($key != $y) {
+				$coordinates[] = array('x' => $x, 'y' => $key);
+			}
+		}
+		return $coordinates;
+	}
+
+	/**
+	 * Get all cells of the current row (given cell excluded)
+	 *
+	 * @param $x
+	 * @param $y
+	 * @return array
+	 */
+	protected function getAllOtherCellsInRow($x, $y) {
+		$coordinates = array();
+		foreach (range(1, 9) as $key) {
+			if ($key != $x) {
+				$coordinates[] = array('x' => $key, 'y' => $y);
+			}
+		}
+		return $coordinates;
+	}
+
+	/**
+	 * Get all cells of the current section (given cell excluded)
+	 *
+	 * @param $x
+	 * @param $y
+	 * @return array
+	 */
+	protected function getAllOtherCellsInSection($x, $y) {
+		$coordinates = array();
+		$xRange = array(
+			floor(($x-1)/3)*3+1,
+			floor(($x-1)/3)*3+3,
+		);
+		$yRange = array(
+			floor(($y-1)/3)*3+1,
+			floor(($y-1)/3)*3+3,
+		);
+		foreach (range($yRange[0], $yRange[1]) as $keyY) {
+			foreach (range($xRange[0], $xRange[1]) as $keyX) {
+				if ($keyX != $x || $keyY != $y) {
+					$coordinates[] = array('x' => $keyX, 'y' => $keyY);
+				}
+			}
+		}
+		return $coordinates;
+	}
+
+	/**
+	 * Check if a value is a candidate for the given cell
+	 *
+	 * @param $x
+	 * @param $y
+	 * @param $candidate
+	 * @return bool
+	 */
+	protected function isCandidateInCell($x, $y, $candidate) {
+		$candidates = $this->getCandidates($x, $y);
+		if (is_array($candidates)) {
+			// echo "Checking if $candidate is a candiate in ($x, $y)<br />";
+			return in_array($candidate, $candidates);
+		} else {
+			if ($this->getValue($x, $y) === $candidate) {
+				return true;
+				// throw new Exception("Trying a candidate that was already set before");
+			}
+			// echo "Checking if $candidate is a candiate in ($x, $y): No array<br />";
+		}
+		return false;
+	}
+
+	/**
+	 * Check if a candidate is unique in a row, cell or section
+	 *
+	 * @param $x
+	 * @param $y
+	 * @param $candidate
+	 * @return bool
+	 */
+	protected function isUniqueCandidate($x, $y, $candidate) {
+		$isUnique = true;
+		// echo "Start checking if $candidate is candiate in column $x<br />";
+		foreach ($this->getAllOtherCellsInColumn($x, $y) as $coordinate) {
+			if ($this->isCandidateInCell($coordinate['x'], $coordinate['y'], $candidate)) {
+				// echo "Value $candidate from ($x, $y) is also candidate in ({$coordinate['x']}, {$coordinate['y']})<br />";
+				$isUnique = false;
+				break;
+			}
+		}
+		if (!$isUnique) {
+			$isUnique = true;
+			// echo "Start checking if $candidate is candiate in row $y<br />";
+			foreach ($this->getAllOtherCellsInRow($x, $y) as $coordinate) {
+				if ($this->isCandidateInCell($coordinate['x'], $coordinate['y'], $candidate)) {
+					// echo "Value $candidate from ($x, $y) is also candidate in ({$coordinate['x']}, {$coordinate['y']})<br />";
+					$isUnique = false;
+					break;
+				}
+			}
+		}
+		if (!$isUnique) {
+			$isUnique = true;
+			// echo "Start checking if $candidate is candiate in section<br />";
+			foreach ($this->getAllOtherCellsInSection($x, $y) as $coordinate) {
+				if ($this->isCandidateInCell($coordinate['x'], $coordinate['y'], $candidate)) {
+					// echo "Value $candidate from ($x, $y) is also candidate in ({$coordinate['x']}, {$coordinate['y']})<br />";
+					$isUnique = false;
+					break;
+				}
+			}
+		}
+		return $isUnique;
+	}
+
+	/**
+	 * Get total count of candidates
+	 *
+	 * @return int
+	 */
+	protected function getTotalCandidateCount() {
 		$candidateCount = 0;
 		foreach (range(1, 9) as $x) {
 			foreach (range(1, 9) as $y) {
@@ -272,56 +390,26 @@ class Board {
 		return $candidateCount;
 	}
 
-	public function logicResolveAsFarAsPossible() {
-		$unsolvedCellCount = 81;
-		$candidateCount = 9*9*9;
-		$iterations = 0;
-
-		while ($unsolvedCellCount != 0) {
-
-			foreach (range(1, 9) as $y) {
-				foreach (range(1, 9) as $x) {
-					if ($this->getValue($x, $y)) {
-						// echo "<h3>Resolving cell($x, $y)</h3>";
-						$this->resolveCell($x, $y);
-						// echo $board->printBoard();
-					}
+	/**
+	 * Search the first candidate (needed for guessing)
+	 *
+	 * @return array
+	 * @throws Exception
+	 */
+	protected function getFirstCandidate() {
+		foreach (range(1, 9) as $y) {
+			foreach (range(1, 9) as $x) {
+				$candidates = $this->getCandidates($x, $y);
+				if (is_array($candidates)) {
+					return array(
+						'x' => $x,
+						'y' => $y,
+						'value' => current($candidates),
+					);
 				}
-			}
-
-			foreach (range(1, 9) as $y) {
-				foreach (range(1, 9) as $x) {
-					$candidates = $this->getCandidates($x, $y);
-					if (is_array($candidates)) {
-						foreach ($candidates as $candidate) {
-							if ($where = $this->isUniqueCandidate($x, $y, $candidate)) {
-								// echo "Candidate $candidate ($x, $y) is no candidate somewhere else in $where and can be set to ($x, $y)<br />";
-								$this->setValue($x, $y, $candidate);
-							}
-						}
-					}
-				}
-			}
-
-			$unsolvedCellCount = $this->getUnsolvedCellCount();
-			$oldCandidateCount = $candidateCount;
-			$candidateCount = $this->getTotalCandidateCount();
-
-			if ($oldCandidateCount == $candidateCount) {
-				echo 'Candidate count did not change. Aborting.<br />';
-				break;
-			}
-
-			$iterations++;
-			if ($iterations > 5) {
-				throw new Exception("Could not solve after $iterations iterations");
 			}
 		}
-		return $candidateCount;
-	}
-
-	public function isSolved() {
-		return ($this->getUnsolvedCellCount() == 0);
+		throw new Exception('No candidate found');
 	}
 
 }
--- index.php
+++ index.php
@@ -1,114 +1,66 @@
 <style type="text/css">
-	body {
-		font-family: Arial;
-	}
-	td {
-		width: 50px;
-		height: 50px;
-		border: 1px solid black;
-		text-align: center;
-		color: #888;
-	}
-	.solved {
-		font-weight: bold;
-		font-size: 25px;
-		color: green;
-	}
-	.row-3, .row-6 {
-		border-bottom-width: 3px;
-	}
-	.column-3, .column-6 {
-		border-right-width: 3px;
-	}
-	table {
-		border-collapse: collapse;
-	}
-
+	body { font-family: Arial; }
+	td { width: 50px; height: 50px; border: 1px solid black; text-align: center; color: #888; }
+	.solved { font-weight: bold; font-size: 25px; color: green; }
+	.row-3, .row-6 { border-bottom-width: 3px; }
+	.column-3, .column-6 { border-right-width: 3px; }
+	table { border-collapse: collapse; border: 3px solid black; }
 </style>
 
 <?php
 
 require_once 'Board.php';
 
-$board = new Board();
-/*
-$board->setValuesAsString('
-..42.79..
-...3.5...
-7...9...4
-61.....29
-..8...5..
-59.....38
-1...6...7
-...5.2...
-..29.36..
-');
-*/
-
-/*
-$board->setValuesAsString('
-.9.....7.
-5..7.8..9
-..1.3.2..
-.7.5.2.6.
-..2...8..
-.8.1.6.4.
-..6.4.5..
-9..8.3..6
-.5.....9.
-');
-*/
-
-$board->setValuesAsString('
-.1...4...
-.6..2....
-..9...7..
-54....9..
-.....3...
-1...42.6.
-.7......2
-....86.53
-..6.5....
-');
-
-$iterations = 0;
-$unsolvedCellCount = $board->getUnsolvedCellCount();
-$candidateCount = 9*9*9;
-
 try {
 
+	$board = new Board();
+
+	/*
+	$board->setValuesAsString('
+	..42.79..
+	...3.5...
+	7...9...4
+	61.....29
+	..8...5..
+	59.....38
+	1...6...7
+	...5.2...
+	..29.36..
+	');
+	*/
+
+	/*
+	$board->setValuesAsString('
+	.9.....7.
+	5..7.8..9
+	..1.3.2..
+	.7.5.2.6.
+	..2...8..
+	.8.1.6.4.
+	..6.4.5..
+	9..8.3..6
+	.5.....9.
+	');
+	*/
+
+	$board->setValuesAsString('
+	.1...4...
+	.6..2....
+	..9...7..
+	54....9..
+	.....3...
+	1...42.6.
+	.7......2
+	....86.53
+	..6.5....
+	');
+
 	while (!$board->isSolved()) {
-		$oldCandidateCount = $candidateCount;
-		$candidateCount = $board->logicResolveAsFarAsPossible();
-
-		$tryBoard = clone $board;
-
-		while (!$tryBoard->isSolved()) {
-			$guess = $tryBoard->getFirstCandidate();
-			echo "Trying {$guess['value']} for ({$guess['x']}, {$guess['y']})<br />";
-			try {
-				$tryBoard->setValue($guess['x'], $guess['y'], $guess['value']);
-				$tryBoard->logicResolveAsFarAsPossible();
-				if ($tryBoard->isSolved()) {
-					$board = $tryBoard;
-				}
-			} catch(Exception $e) {
-				echo "Did not work.<br />";
-				$board->removeCandidate($guess['x'], $guess['y'], $guess['value']);
-				break;
-			}
-		}
-
-		$iterations++;
-		if ($iterations > 5) {
-			throw new Exception("Could not solve after $iterations iterations");
-		}
+		$board->logicResolveAsFarAsPossible();
+		$board->guess();
 	}
-
 } catch (Exception $e) {
-
 	echo "ERROR: " . $e->getMessage() . '<br />';
-
 }
 
 echo $board->printBoard();
